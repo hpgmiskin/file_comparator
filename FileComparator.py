fd0aa3ca156f60f21a089bb1697518d36f44ced3
Henry Miskin <henry.miskin@talktalk.net>
2013-10-23 19:35:49 +0100

    Initial Commit

---
+++ FileComparator.py
@@ -0,0 +1,24 @@
+#FileComparator.py
+import re
+
+#import FolderDuplicates,SanitiseString
+
+class FolderDuplicates():
+	"""FolderDuplicates provides the logic to compare the contents any number of folder to find duplicates"""
+
+	def __init__(self):
+		print("SETUP")
+
+
+class SanitiseString():
+	"""SanitiseString provides the logic to remove unwanted content from string input"""
+
+	def __init__(self,stringInput):
+		self.string = stringInput
+
+	def removeCharacters(characters):
+		"method to strip class string of certain characters"
+		
+
+if __name__ == "__main__":
+	print("running")
