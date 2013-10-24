15ceae70faf5388773bcfa8d9b8059f91ae07b34
Henry Miskin <henry.miskin@talktalk.net>
2013-10-24 17:51:21 +0100

    Added removeBetween method

--- FileComparator.py
+++ FileComparator.py
@@ -19,27 +19,43 @@ class SanitiseString():
 	def printString(self):
 		print(self.string)
 
-	def removeCharacters(self,characters):
-		"method to strip class string of certain characters"
+
+	def removeBetween(self,openCharacter,closeCharacter):
+		"method to remove the string between the given characters and the characters themselves"
+
+		oldString = self.string
+		pattern = re.compile("(\{}.*\{})".format(openCharacter,closeCharacter))
+		matches = re.findall(pattern,oldString)
+		self.replaceCharacters(matches)
+
+		return matches
+
+	def replaceCharacters(self,characters,substitute=""):
+		"method to replace given characters in the class string with the given substitute"
+
+		if (len(characters) < 1):
+			return None
 
 		oldString = self.string
 
-		#pattern = re.escape(character)
-
 		for character in characters:
 			pattern = re.escape(character)
-			newString = re.sub(pattern, "", oldString)
+			newString = re.sub(pattern, substitute, oldString)
 			oldString = newString
-			#print(newString)
 
 		self.string = newString
-
-
+		return newString
 		
 
 if __name__ == "__main__":
 
-	sanitiseString = SanitiseString("""Test ()""??,,*""")
+	sanitiseString = SanitiseString("""Test 'comma' [square] (bracket)""??,,*""")
 	sanitiseString.printString()
-	sanitiseString.removeCharacters(["?",",","*","\"","(",")"])
+	sanitiseString.replaceCharacters(["?",",","*","\""])
+	sanitiseString.printString()
+	sanitiseString.removeBetween("(",")")
+	sanitiseString.printString()
+	sanitiseString.removeBetween("[","]")
+	sanitiseString.printString()
+	sanitiseString.removeBetween("'","'")
 	sanitiseString.printString()
